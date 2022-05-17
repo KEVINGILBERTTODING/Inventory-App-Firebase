09a493166256c8c94ca615d552d8f61c46763686
Kevin Gilbert Toding <122202002832@mhs.dinus.ac.id>
2022-05-17 08:00:21 +0800

    Added SplashScreen :deciduous_tree :blossom:

---
+++ app/src/main/java/com/example/firebasebarang/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.example.firebasebarang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // Fungsi untuk menyembunyikan navbar
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        setContentView(R.layout.activity_splash_screen);
+
+
+        // Fungsi untuk menyembunyikan status bar
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 4000L);
+    }
+}
